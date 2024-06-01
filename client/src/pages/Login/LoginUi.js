416c1b5406c8032315c3cc89ec2524541ed967d6
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-06-01 19:49:36 +0300

    Update LoginUi.js

--- client/src/pages/Login/LoginUi.js
+++ client/src/pages/Login/LoginUi.js
@@ -170,7 +170,7 @@ function Login() {
                   <div className="relative flex items-center justify-center">
                     <img
                       src="https://tailus.io/sources/blocks/social/preview/images/google.svg"
-                      className="absolute left-5 w-5"
+                      className="absolute left-0 w-5"
                       alt="google logo"
                     />
                     <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
