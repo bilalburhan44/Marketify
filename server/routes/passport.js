019d69ea84a678c4b9a3aaf7dc18b22adf738cd6
bilalburhan44 <110193290+bilalburhan44@users.noreply.github.com>
2024-06-01 16:38:10 +0300

    Update passport.js

--- server/routes/passport.js
+++ server/routes/passport.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/google/callback"
+  callbackURL: "https://marketify-qcnh.onrender.com/auth/google/callback"
 },
 function(accessToken, refreshToken, profile, done) {
   const { id, emails, displayName, photos } = profile;
